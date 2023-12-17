bd070604b2fc2a7e9a7a25667f3de5a0100959d0
Jason Selau Kumar <148664294+Mecha-Coder@users.noreply.github.com>
2023-12-17 17:09:11 +0800

    Rev 01
    Initial

---
+++ src/components/Error.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Error (){
+  return <h1>There is an error in your address</h1>
+}
+
+export default Error;
---
+++ src/components/Price.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function Price (){
+  return <h1>This is price page</h1>
+}
+
+export default Price;
